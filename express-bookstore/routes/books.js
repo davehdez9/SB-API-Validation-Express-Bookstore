1fec160a1ac554cd007aa17f6b3eb74d9c923fc7
David <david@Davids-MacBook-Pro.local>
2021-01-19 18:12:47 -0500

    Validation

--- express-bookstore/routes/books.js
+++ express-bookstore/routes/books.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const Book = require("../models/book");
-
 const router = new express.Router();
+const ExpressError = require("../expressError")
+
+const jsonschema = require("jsonschema")
+const bookSchema = require("../schemas/bookSchema.json")
+
+const Book = require("../models/book");
 
 
 /** GET / => {books: [book, ...]}  */
@@ -27,20 +31,39 @@ router.get("/:id", async function (req, res, next) {
 });
 
 /** POST /   bookData => {book: newBook}  */
+/**[] Implement validation for create a book - Display error messages containing all of the validation errors if books creations fails */
 
 router.post("/", async function (req, res, next) {
+  
   try {
-    const book = await Book.create(req.body);
+    const validation = jsonschema.validate(req.body, bookSchema)
+    if(!validation.valid){
+      const listOfErrors = validation.errors.map(e => e.stack)
+      const err = new ExpressError(listOfErrors, 400)
+      return next(err)
+    }
+    const book = await Book.create(req.body)
     return res.status(201).json({ book });
-  } catch (err) {
-    return next(err);
+  } catch (error) {
+    return next(error)
   }
 });
 
 /** PUT /[isbn]   bookData => {book: updatedBook}  */
+/**[] Implement validation for Update a book - Display error messages containing all of the validation errors if books update fails */
 
 router.put("/:isbn", async function (req, res, next) {
   try {
+    // if("isbn" in req.body){
+    //   const err = new ExpressError("Not allowed to Edit", 400)
+    //   return next(err)
+    // }
+    const validation = jsonschema.validate(req.body, bookSchema)
+    if(!validation.valid){
+      const listOfErrors = validation.errors.map(e => e.stack)
+      const err = new ExpressError(listOfErrors, 400)
+      return next(err)
+    }
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
   } catch (err) {
